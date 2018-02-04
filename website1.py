f2ba34ab121484b94de18ebacff50cf7f24d6a7e
saladln <aytac125@gmail.com>
2018-02-04 23:57:33 +0300

    adding dictonary.html

--- website1.py
+++ website1.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/dictionary/")
+def about():
+    return render_template("dictionary.html")
+
 if __name__=="__main__":
     app.run(debug=True)
